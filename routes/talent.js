e251cbcf559d36bfb81d2216875a400b5ae35e79
Antoinette Smith <antoinette@primeacademy.io>
2015-12-14 09:41:27 -0600

    FOrgot to add additions

--- routes/talent.js
+++ routes/talent.js
@@ -2,9 +2,24 @@ var express = require('express');
 var router = express.Router();
 var Talent = require('../models/talent');
 
-/* GET talent listing. */
+/* GET all talent. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // TODO: Find all talent documents and send them back as an array of objects
+});
+
+/* Add a talent record. */
+router.post('/', function(req, res, next) {
+  // TODO: Add a talent document that you've received from the client
+});
+
+/* Add a history object to a talent record by ID. */
+router.put('/history/:id', function(req, res, next) {
+  // TODO: Find the talent record and update its history
+});
+
+/* Delete talent by ID. */
+router.delete('/:id', function(req, res, next) {
+  // TODO: Find one talent document by ID and remove it
 });
 
 module.exports = router;
