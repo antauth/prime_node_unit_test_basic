045ab22ef6bd4232cbd85f6f79bd10a13bb8c4e0
Antoinette Smith <antoinette@primeacademy.io>
2016-01-26 19:44:07 -0600

    Added a space

--- models/talent.js
+++ models/talent.js
@@ -10,7 +10,7 @@ var Schema = mongoose.Schema; // mongoose schema for defining our models
 // Step 1 - Create the schema
 var talentSchema = new Schema({
   talent_id: Number,
-  name: {type:String, required:true},
+  name: {type:String, required: true},
   address: String,
   skills: [String],
   wage_req: [{
